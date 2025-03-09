fc075f6dfcfb3c7ea1fa20b7d4b87d3727eedd83
dmarshaq <mrshovdaniil@gmail.com>
2025-03-09 19:22:45 -0400

    Quality of life fix.

--- dmarshaq-setup/AppData/Local/nvim/lua/dmarshaq/config/set.lua
+++ dmarshaq-setup/AppData/Local/nvim/lua/dmarshaq/config/set.lua
@@ -8,6 +8,10 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.list = false
+vim.opt.listchars = { space = '·'}
+
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = true
@@ -35,26 +39,26 @@ vim.opt.scrolloff = 10
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+    vim.highlight.on_yank()
+    end,
 })
 
 -- get contents of visual selection
 -- handle unpack deprecation
 table.unpack = table.unpack or unpack
 function get_visual()
-	local _, ls, cs = table.unpack(vim.fn.getpos("v"))
-	local _, le, ce = table.unpack(vim.fn.getpos("."))
-	return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
+    local _, ls, cs = table.unpack(vim.fn.getpos("v"))
+    local _, le, ce = table.unpack(vim.fn.getpos("."))
+    return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
 end
 
 vim.keymap.set("v", "<leader>r", function()
-	local pattern = table.concat(get_visual())
-	-- escape regex and line endings
-	pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
-	-- send parsed substitution command to command line
-	vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//<Left>")
+    local pattern = table.concat(get_visual())
+    -- escape regex and line endings
+    pattern = vim.fn.substitute(vim.fn.escape(pattern, "^$.*\\/~[]"), "\n", "\\n", "g")
+    -- send parsed substitution command to command line
+    vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//<Left>")
 end)
--- dmarshaq-setup/AppData/Local/nvim/lua/dmarshaq/plugins/scroll.lua
+++ dmarshaq-setup/AppData/Local/nvim/lua/dmarshaq/plugins/scroll.lua
@@ -1,7 +1,7 @@
 return {
-	"karb94/neoscroll.nvim",
-	config = function()
-		require("neoscroll").setup({ mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" } })
-	end,
-	opts = {},
+    "karb94/neoscroll.nvim",
+    config = function()
+        require("neoscroll").setup({ mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>" } })
+    end,
+    opts = {},
 }
